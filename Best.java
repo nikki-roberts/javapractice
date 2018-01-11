ca4af00ab9d13337ac13e44e661eba64d3bf5aed
Nikki Roberts <robertsni@s.dcsdk12.org>
2018-01-11 11:58:55 -0700

    add optional arg to print more ga .!

--- Best.java
+++ Best.java
@@ -1,16 +1,22 @@
-import java.util.Scanner;
-
 class Best {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("Type a number: ");
-		int x = scanner.nextInt();
-
-		for (int i=0; i<x; i++) {
-			System.out.println("YOU'RE THE BEST!!");
-		}
+		if (args.length > 1) {
+			int x = Integer.parseInt(args[0]);
+			int y = Integer.parseInt(args[1]);
+			String exclamation = "";
+			for (int i=0; i<y; i++) {
+				exclamation = exclamation + "!";
+			}
+			for (int i=0; i<x; i++) {
+				System.out.println("YOU'RE THE BEST!" + exclamation );
+			}
+		}  else {
+			int x = Integer.parseInt(args[0]);
+			for (int i=0; i<x; i++) {
+				System.out.println("YOU'RE THE BEST!");
+			}
+		}	
 	}
 
 }
