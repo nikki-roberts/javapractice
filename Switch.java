e4af073e560fe8f38fc964b3ff6d29d0f65a79dd
Nikki Roberts <robertsni@s.dcsdk12.org>
2018-01-16 11:15:21 -0700

    create swtich file

---
+++ Switch.java
@@ -0,0 +1,28 @@
+class Switch {
+
+	public static void main (Strings[] args) {
+
+	}
+
+	public static String switcher(String initial, String swapA, String swapB) {
+		// loop thru initial in segments of the length of swapA
+		//		look for spots the same as swapA
+		//		if i find one, replace tge current segment with a temp swap string
+		for (; ; ) {
+			
+		}
+		// loop thru initial in segments of the length of swapB
+		//		look for spots the same as swapB
+		//		if i find one, replace the current segment with a swapA
+		for (; ; ) {
+			
+		}
+		// loop thru initial in segments of the length of the temp swap string
+		//		look for spots the same as temp
+		//		if i find one, replace tge current segment with a swapB
+		for (; ; ) {
+			
+		}
+	}
+
+}
